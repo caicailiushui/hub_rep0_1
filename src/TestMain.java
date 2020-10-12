ec9d1ba582f1b510fb9e9e5a90a6225bda204838
caicailiushui <wang.jian@sysystem.com.cn>
2020-10-12 15:11:29 +0800

    test

---
+++ src/TestMain.java
@@ -0,0 +1,12 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		String a="hello";
+		String b="hello";
+		System.out.println(a.compareTo(b)==0);
+		
+		System.out.println(a.equals(b));
+	}
+
+}
